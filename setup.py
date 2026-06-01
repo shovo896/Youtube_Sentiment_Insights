782af52f7354e0af554a6b2e81b826d7e3a8284b
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-06-02 01:48:20 +0600

    dfijnjl

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'bombox',
+    version= '0.0.0',
+    author= 'Shovo',
+    author_email= 'suvo10.info@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
